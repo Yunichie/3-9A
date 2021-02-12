a92facd255cf70e74627840a09276eb18d46824b
Yunichi <alfarelsandriano@gmail.com>
2021-02-12 08:18:01 +0000

    (?)

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function equal() {
   document.c.txt.value = eval(c.txt.value); 
-  if(c.txt.value.length > 15) { alert('input tidak bisa lebih dari 15 karakter!'); 
-  document.c.txt.value = ''; 
+  if(c.txt.value.length > 15) {
+    alert('input tidak bisa lebih dari 15 karakter!'); 
+    document.c.txt.value = ''; 
   }
 }
 
 function smallC() {
-  document.c.txt.value = c.txt.value.replace(/([^])$/g, '')
+  document.c.txt.value = c.txt.value.replace(/([^])$/g, '');
 }
